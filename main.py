f0a9711a8abf78ea3cf1a9b712a954f0e42d220e
buttumaklevit <106801888+buttumaklevit@users.noreply.github.com>
2024-11-29 18:24:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ with st.sidebar:
         desease_result = "없음" if len(disease)==0 else disease
 
         new_user = [name, age, height, weight, desease_result]
+        user_setting = st.button()
 
     openai_api_key = st.text_input("OpenAI API Key", type="password")
     pass
